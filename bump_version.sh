7a43a91dd8f5817a19a83837ca1eaee859dfcee6
Ersin KAYA <ersinkaya@anadolu.edu.tr>
2024-02-28 13:55:45 +0300

    script eklendi

---
+++ bump_version.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Mevcut versiyon numarasını al
+current_version=$(grep 'VERSION' Dockerfile | sed -n 's/.*VERSION="\([^"]*\)".*/\1/p')
+
+# Versiyon numarasını artır
+new_version=$(echo $current_version | awk -F. '{$NF = $NF + 1;} 1' OFS=. )
+
+# Dockerfile'daki versiyon numarasını güncelle
+sed -i.bak "s/VERSION=\"$current_version\"/VERSION=\"$new_version\"/g" Dockerfile
+
+# Yeni versiyon numarasını çıktı olarak yazdır
+echo $new_version
