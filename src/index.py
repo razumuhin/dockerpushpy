4daa74f700614f774f8e1966df7c78d36e25fc0c
ersinkaya <ersinkayaqanadolu.edu.tr>
2024-03-01 14:51:40 +0300

    qqq

--- src/index.py
+++ src/index.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Merhaba seeeeonunda22 naber?"
+    return "Merhaba seeeeonundassasasa22 naber?"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
