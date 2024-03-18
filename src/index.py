492df87c0718b46de914ea40d5410d59f9ea4cdf
ersinkaya <ersinkayaqanadolu.edu.tr>
2024-03-18 15:55:04 +0300

    değişiklik

--- src/index.py
+++ src/index.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Merhaba Ersin naber?"
+    return "Merhaba Ersin  KAYA naber?  Bu değişiklik 18 öart 2024 tarihinde yapıldı."
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
